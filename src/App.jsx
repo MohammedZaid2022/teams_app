d699e53fc940b665b4099e1c613c3041423fea48
Mohammed Zaid F <mohammed.z@zerozilla.com>
2025-01-02 17:02:37 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
 import Home from "./components/Home";
 import PrivacyPolicy from "./components/PrivacyPolicy";
-import TermsOfUse from "./components/PrivacyPolicy";
+import TermsOfUse from "./components/TermsOfUse";
 
 function App() {
   return (
